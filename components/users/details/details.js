da44829a8e623b673258c98f8ac817992c689daf
Arikaim <sales@intersoft.biz>
2020-05-25 09:47:46 +0300

    =

--- components/users/details/details.js
+++ components/users/details/details.js
@@ -3,10 +3,10 @@
 $(document).ready(function() {
     arikaim.ui.form.onSubmit('#user_details_form',function() {
        return users.changeDetails('#user_details_form');
-    },function(result) {       
+    },function(result) {        
         arikaim.ui.form.showMessage({           
             message: result.message
         });
-    },function(error) {
+    },function(error) {        
     });
 });
--- components/users/details/form/form.js
+++ components/users/details/form/form.js
@@ -3,14 +3,16 @@
 $(document).ready(function() { 
     arikaim.ui.viewPasswordButton('.view-password');
     arikaim.ui.form.addRules("#user_details_form",{
-        inline: false,
+        inline: false,          
         fields: {
             user_name: {
+                indentifier: 'user_name', 
                 rules: [{ type: 'minLength[2]' }]
             },
             email: {
+                indentifier: 'email', 
                 rules: [{ type: 'email' }]
             }
         }
-    });  
+    });      
 });

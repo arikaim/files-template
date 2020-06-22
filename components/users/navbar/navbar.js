c541f6065600f8dda2762ea9f3f6d4ef2e70ca61
Arikaim <sales@intersoft.biz>
2020-06-22 17:54:40 +0300

    =

--- components/users/navbar/navbar.js
+++ components/users/navbar/navbar.js
@@ -1,8 +1,6 @@
 'use strict';
 
 $(document).ready(function() {
-    $('.mobile-menu').dropdown({});
-    
     $('#language_dropdown').dropdown({
         onChange: function(value) {       
             arikaim.setLanguage(value);
